6d9c60d2e8265b072e05ae27166fc8ae9445a9b5
Carlos <carlos.munoz.hernandez@gmail.com>
2023-09-11 23:05:45 -0400

    Ejercicio Carlos Muñoz

---
+++ app/src/main/java/ejercicios/carlosmunoz.kt
@@ -0,0 +1,8 @@
+package ejercicios
+fun main (){
+
+
+    println(esPar(4))
+    println(esPar(3))
+}
+fun esPar(numero:Int) = numero % 2 ==0
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "TEST"
 include(":app")
+include(":app")
+ 
